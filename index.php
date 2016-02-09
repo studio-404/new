ba044be36654d1613e7b2f0c525db904ba760615
gio <hosting@404.ge>
2016-02-09 17:38:25 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ge">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test</title>
+	<!-- FB Meta tags (start) -->
+	<meta property="fb:app_id" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:type" content="" />
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content="" />
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<!-- FB Meta tags (end)-->
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="" />
+	<!--BOOTSTRAP start-->
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/general.css" type="text/css" />
+	<link rel="stylesheet" href="css/ge.css" type="text/css" />
+	<script src="js/jquery-1.12.0.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+	<!--BOOTSTRAP end-->
+</head>
+<body>
+
+
+<script src="js/scripts.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
